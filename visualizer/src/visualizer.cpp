f9de9e3c4d83e735526e8d26b24adbd62a9e68f8
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2020-12-28 11:21:23 +0100

    Add header file for visualizer
    Make header file a dependency for visualizer target

--- visualizer/src/visualizer.cpp
+++ visualizer/src/visualizer.cpp
@@ -13,38 +13,7 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 // See README and LICENSE for more details.
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-#include <string>
-
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-#include "allsorts.h"
-
-VISUALIZER_SWAP* visualizer_itemsSwapped;
-VISUALIZER_ADV* visualizer_pointerAdvanced;
-
-int arraySize = 0;
-int* array = nullptr;
-
-int dataMax = 100;
-unsigned int shaderProg, vis_tex, vis_vao, vis_ebo, vis_fbo;
-float* vis_vertices;
-unsigned int* vis_indices;
-
-const char* sequences_s[] = {
-	"Shell 1959", "Frank & Lazarus 1960",
-	"A168604", "A083318", "A003586", "A003462", "A036569", "A036562", "A033622",
-	"Gonnet & Baeza-Yates 1991",
-	"A108870", "A102549"
-};
+#include "visualizer.h"
 
 void renderVisualizer() {
 	float vertices[12];
---
+++ visualizer/src/visualizer.h
@@ -0,0 +1,52 @@
+// Copyright (C) 2020 Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation (version 3)
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// See README and LICENSE for more details.
+
+#ifndef VISUALIZER_H
+#define VISUALIZER_H
+
+#include <cstdlib>
+#include <ctime>
+
+#include <iostream>
+#include <string>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "allsorts.h"
+
+VISUALIZER_SWAP* visualizer_itemsSwapped;
+VISUALIZER_ADV* visualizer_pointerAdvanced;
+
+int arraySize = 0;
+int* array = nullptr;
+
+int dataMax = 100;
+unsigned int shaderProg, vis_tex, vis_vao, vis_ebo, vis_fbo;
+float* vis_vertices;
+unsigned int* vis_indices;
+
+const char* sequences_s[] = {
+	"Shell 1959", "Frank & Lazarus 1960",
+	"A168604", "A083318", "A003586", "A003462", "A036569", "A036562", "A033622",
+	"Gonnet & Baeza-Yates 1991",
+	"A108870", "A102549"
+};
+
+#endif
